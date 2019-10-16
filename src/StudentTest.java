ab1122b462005801d7174fd4e0491e290bcc79ec
Matthew Castle <matthew.g.castle-1@ou.edu>
2019-10-16 14:17:05 -0500

    First commit after clone from github

---
+++ src/StudentTest.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentTest {
+
+	private static final int INITIAL_RATING = 5;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testStudent() {
+		try {
+			Student s = new Student();
+
+			Integer rating = s.getRating();
+			Integer expected = INITIAL_RATING;
+
+			assertEquals(expected, rating);
+		} catch (NameException e) {
+			fail();
+		}
+	}
+
+	@Test
+	public void testCompareTo() throws NameException {
+		Student firstStudent = new Student("x", "Y", 2);
+		Student secondStudent = new Student("x", "Y", 3);
+		
+		int actual = secondStudent.compareTo(firstStudent);
+		// Should be bigger than 0
+		boolean result = false;
+		if(actual > 0) {
+			result = true;
+		}
+		assertTrue(result);
+		
+		// Should be less than zero
+		Student student = new Student("x", "Y", 5);
+		result = false;
+		
+		actual = secondStudent.compareTo(student);
+		assertTrue(actual < 0);
+
+
+		
+		
+		
+
+
+	}
+	
+	@Test
+	public void testGet() throws NameException {
+		Integer expected = 4;
+		Integer rating = expected;
+		String firstName = "Alex";
+		String lastName = "Bo";
+		Student s = new Student(firstName, lastName, rating);
+		Integer actual = s.getRating();
+		assertEquals(expected, actual);
+	}
+
+}
